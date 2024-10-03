626c861eccf5439c20517030cd988136c9e923b2
divyanshususa <155217772+divyanshususa@users.noreply.github.com>
2024-10-03 11:48:21 +0530

    Update config.jsx
    qwerty

--- frontend/src/configuration/config.jsx
+++ frontend/src/configuration/config.jsx
@@ -2,7 +2,9 @@
 
 const config = {
     // baseURL: 'https://denave-1705124151487.df.r.appspot.com', // Change this to your base URL
-        baseURL: 'http://localhost:5000',
+        // baseURL: 'http://localhost:5000',
+       baseURL: ' https://hrms-deployed.onrender.com',
+   
         TicketProjectUrl : 'https://melodic-arcadia-417919.web.app'
     // Other configuration options...
   };
